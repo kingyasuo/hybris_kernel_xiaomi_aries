6507889ddd1b558d4f78ca195584aee40bbc3aa7
Linus Torvalds <torvalds@linux-foundation.org>
2015-08-31 05:34:52 -0400

    Revert "kgsl_pwrscale: Revive msm and conservative governors"
    This reverts commit 44b5a59b1264bc5196c01e18caf7a57891fe9442.

--- drivers/gpu/msm2/kgsl_pwrscale.c
+++ drivers/gpu/msm2/kgsl_pwrscale.c
@@ -48,10 +48,6 @@ static struct kgsl_pwrscale_policy *kgsl_pwrscale_policies[] = {
 #ifdef CONFIG_MSM_SLEEP_STATS_DEVICE
 	&kgsl_pwrscale_policy_idlestats,
 #endif
-#ifdef CONFIG_MSM_DCVS
-	&kgsl_pwrscale_policy_msm,
-#endif
-	&kgsl_pwrscale_policy_conservative,
 	NULL
 };
 
@@ -270,9 +266,6 @@ int kgsl_pwrscale_policy_add_files(struct kgsl_device *device,
 {
 	int ret;
 
-	kobject_del(&pwrscale->kobj);
-	kobject_put(&pwrscale->kobj);
-
 	ret = kobject_add(&pwrscale->kobj, &device->pwrscale_kobj,
 		"%s", pwrscale->policy->name);
 
@@ -280,6 +273,11 @@ int kgsl_pwrscale_policy_add_files(struct kgsl_device *device,
 		return ret;
 
 	ret = sysfs_create_group(&pwrscale->kobj, attr_group);
+
+	if (ret) {
+		kobject_del(&pwrscale->kobj);
+		kobject_put(&pwrscale->kobj);
+	}
 
 	return ret;
 }
@@ -289,6 +287,8 @@ void kgsl_pwrscale_policy_remove_files(struct kgsl_device *device,
 				       struct attribute_group *attr_group)
 {
 	sysfs_remove_group(&pwrscale->kobj, attr_group);
+	kobject_del(&pwrscale->kobj);
+	kobject_put(&pwrscale->kobj);
 }
 
 static void _kgsl_pwrscale_detach_policy(struct kgsl_device *device)
